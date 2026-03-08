83748335bb7a5d6134854b01de97080b8161f130
lezziesalt <lizzie.salinas2025@gmail.com>
2026-03-08 18:11:06 -0500

    Update server.py

--- server.py
+++ server.py
@@ -3,8 +3,16 @@ import json
 
 app = Flask(__name__)
 
+# load search database
 with open("database.json") as f:
     pages = json.load(f)
+
+# load bookmarks
+try:
+    with open("bookmarks.json") as f:
+        bookmarks = json.load(f)
+except:
+    bookmarks = {}
 
 @app.route("/search")
 def search():
@@ -17,5 +25,27 @@ def search():
             results.append(page)
 
     return jsonify(results)
+
+@app.route("/bookmark", methods=["POST"])
+def bookmark():
+
+    ip = request.remote_addr
+    data = request.json
+
+    if ip not in bookmarks:
+        bookmarks[ip] = []
+
+    bookmarks[ip].append(data)
+
+    with open("bookmarks.json","w") as f:
+        json.dump(bookmarks,f)
+
+    return {"status":"saved"}
+
+@app.route("/bookmarks")
+def get_bookmarks():
+
+    ip = request.remote_addr
+    return jsonify(bookmarks.get(ip, []))
 
 app.run(port=5000)
